998bbfa31f5f58f32651be7e905fe037bd0e3713
Christopher <ChrFimbel@gmail.com>
2023-06-23 11:44:23 -0600

    Create sax.ts

---
+++ src/weapons/toan/sax.ts
@@ -0,0 +1,13 @@
+import Weapon from '../Weapon';
+
+const chopper = new Weapon({
+  antiMimic: { min: 10 },
+  antiPlant: { min: 10 },
+  attack: { max: 65, min: 26 },
+  endurance: { min: 40 },
+  magicalPower: { max: 41, min: 15 },
+  speed: { min: 50 },
+  thunder: { min: 10 },
+});
+
+export default chopper;
