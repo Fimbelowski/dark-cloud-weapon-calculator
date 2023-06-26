06ae692a93f76b21e52708b2a8c4f97410ab9a61
Christopher <ChrFimbel@gmail.com>
2023-06-26 12:38:34 -0600

    Add defaultWeapon property.

--- src/weapons/Weapon.ts
+++ src/weapons/Weapon.ts
@@ -4,11 +4,13 @@ type WeaponName = SwordName;
 
 export default abstract class Weapon<T extends WeaponName> {
   buildsUpInto: T[];
+  defaultWeapon: boolean;
   name: T;
 
-  constructor(name: T, buildsUpInto: T[] = []) {
+  constructor(name: T, buildsUpInto: T[] = [], defaultWeapon = false) {
     {
       this.buildsUpInto = buildsUpInto;
+      this.defaultWeapon = defaultWeapon;
       this.name = name;
     }
   }
