a5c649c8a1af41dc7cce0f7ebc21f078c7b08d96
Christopher <ChrFimbel@gmail.com>
2023-07-12 11:18:41 -0600

    Create weapon-container component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WeaponGraphComponent } from './weapon-graph/weapon-graph.component';
 import { WeaponComponent } from './weapon/weapon.component';
+import { WeaponContainerComponent } from './weapon-container/weapon-container.component';
 
 @NgModule({
-  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent],
+  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent, WeaponContainerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/weapon-container/weapon-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weapon-container',
+  templateUrl: './weapon-container.component.html',
+  styleUrls: ['./weapon-container.component.scss']
+})
+export class WeaponContainerComponent {
+
+}
