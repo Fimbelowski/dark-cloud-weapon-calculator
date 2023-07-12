42d25eec93c2636fd29421c9bf5962d2beef7825
Christopher <ChrFimbel@gmail.com>
2023-07-12 11:36:51 -0600

    Revert "Create weapon-container component."
    This reverts commit a5c649c8a1af41dc7cce0f7ebc21f078c7b08d96.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WeaponGraphComponent } from './weapon-graph/weapon-graph.component';
 import { WeaponComponent } from './weapon/weapon.component';
-import { WeaponContainerComponent } from './weapon-container/weapon-container.component';
 
 @NgModule({
-  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent, WeaponContainerComponent],
+  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
