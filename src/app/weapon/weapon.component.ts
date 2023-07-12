f4818da09b1b02a77f2209b879bdc07545433435
Christopher <ChrFimbel@gmail.com>
2023-07-12 10:41:00 -0600

    Add weapon input to weapon component.

--- src/app/weapon/weapon.component.ts
+++ src/app/weapon/weapon.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import type Weapon from 'src/services/weapon/Weapon';
+import type WeaponName from 'src/services/weapon/WeaponName';
 
 @Component({
   selector: 'app-weapon',
   templateUrl: './weapon.component.html',
-  styleUrls: ['./weapon.component.scss']
+  styleUrls: ['./weapon.component.scss'],
 })
-export class WeaponComponent {
-
+export class WeaponComponent<T extends WeaponName> {
+  @Input({ required: true }) weapon!: Weapon<T>;
 }
