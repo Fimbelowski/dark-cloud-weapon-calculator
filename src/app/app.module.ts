f1740eca3214875cd99b1f52b6484d174e5dde57
Christopher <ChrFimbel@gmail.com>
2023-07-17 11:42:35 -0600

    Create edge component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WeaponGraphComponent } from './weapon-graph/weapon-graph.component';
 import { WeaponComponent } from './weapon/weapon.component';
+import { EdgeComponent } from './edge/edge.component';
 
 @NgModule({
-  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent],
+  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent, EdgeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
