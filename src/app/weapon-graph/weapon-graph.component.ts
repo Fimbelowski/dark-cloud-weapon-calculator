95b8bd5ba00cbd6a8e018208e0f57cd0e36b7268
Christopher <ChrFimbel@gmail.com>
2023-07-17 11:42:27 -0600

    Add logic for building a set of all edges.

--- src/app/weapon-graph/weapon-graph.component.ts
+++ src/app/weapon-graph/weapon-graph.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import type Weapon from 'src/services/weapon/Weapon';
 import WeaponGraph from 'src/services/weapon/WeaponGraph';
 import type WeaponName from 'src/services/weapon/WeaponName';
+import type WeaponNameByType from 'src/services/weapon/WeaponNameByType';
 
 @Component({
   selector: 'app-weapon-graph',
@@ -12,10 +13,27 @@ import type WeaponName from 'src/services/weapon/WeaponName';
 export class WeaponGraphComponent<T extends WeaponName> implements OnInit {
   @Input({ required: true }) weaponGraph!: Readonly<WeaponGraph<T>>;
 
+  edges = new Set<[WeaponNameByType[T], WeaponNameByType[T]]>();
   styles = new Map<string, string>();
   weaponsMatrix: Array<Array<Weapon<T>>> = [];
 
   ngOnInit() {
+    this.buildWeaponsMatrix();
+    this.buildEdges();
+    this.setStyles();
+  }
+
+  buildEdges() {
+    this.weaponGraph.adjacencyList.forEach((value, key) => {
+      value.forEach((potentialBuildUp) => {
+        this.edges.add([key.name, potentialBuildUp.name]);
+      });
+    });
+
+    console.log(this.edges);
+  }
+
+  buildWeaponsMatrix() {
     this.weaponGraph.vertices.forEach((weapon) => {
       const distanceFromTerminal =
         this.weaponGraph.getVertexDistanceFromTerminal(weapon);
@@ -25,7 +43,9 @@ export class WeaponGraphComponent<T extends WeaponName> implements OnInit {
 
       this.weaponsMatrix[distanceFromTerminal].push(weapon);
     });
+  }
 
+  setStyles() {
     const columns = Math.max(...this.weaponsMatrix.map((row) => row.length));
     const rows = this.weaponsMatrix.length;
 
