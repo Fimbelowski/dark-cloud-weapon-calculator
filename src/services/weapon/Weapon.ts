777a4b5a17c9a98258ab75bc7567c24d5b31b946
Christopher <ChrFimbel@gmail.com>
2023-07-26 11:34:36 -0600

    Update Weapon.ts

--- src/services/weapon/Weapon.ts
+++ src/services/weapon/Weapon.ts
@@ -4,15 +4,18 @@ import type WeaponName from './WeaponType';
 export default abstract class Weapon<T extends WeaponName> {
   buildsUpInto: Set<Weapon<T>>;
   defaultWeapon: boolean;
+  icons: string[];
   name: WeaponNameByType[T];
 
   constructor(
     name: WeaponNameByType[T],
+    iconOrIcons: string | string[],
     buildsUpInto = new Set<Weapon<T>>(),
     defaultWeapon = false
   ) {
     this.buildsUpInto = buildsUpInto;
     this.defaultWeapon = defaultWeapon;
+    this.icons = typeof iconOrIcons === 'string' ? [iconOrIcons] : iconOrIcons;
     this.name = name;
   }
 }
