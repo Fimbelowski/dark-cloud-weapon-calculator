e89e9067e6c95f552a9dbb9e4d21d889b82e5e68
Christopher <ChrFimbel@gmail.com>
2023-07-27 12:58:43 -0600

    Update Sword.ts

--- src/services/weapon/sword/Sword.ts
+++ src/services/weapon/sword/Sword.ts
@@ -1,15 +1,9 @@
 import Weapon from '../Weapon';
-import type SwordName from './SwordName';
 
 export default class Sword extends Weapon<'sword'> {
   ICON_URL_FOLDER = 'swords';
 
-  constructor(
-    name: SwordName,
-    iconPathOrPaths: string | string[],
-    buildUpInto?: Set<Sword>,
-    defaultWeapon = false
-  ) {
-    super(name, iconPathOrPaths, buildUpInto, defaultWeapon);
+  constructor(...args: ConstructorParameters<typeof Weapon<'sword'>>) {
+    super(...args);
   }
 }
