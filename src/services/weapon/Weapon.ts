d309a3ce5f1f48609690013d3c24a9238cb2676b
Christopher <ChrFimbel@gmail.com>
2023-07-27 11:45:55 -0600

    Update Weapon.ts

--- src/services/weapon/Weapon.ts
+++ src/services/weapon/Weapon.ts
@@ -4,18 +4,29 @@ import type WeaponName from './WeaponType';
 export default abstract class Weapon<T extends WeaponName> {
   buildsUpInto: Set<Weapon<T>>;
   defaultWeapon: boolean;
-  icons: string[];
+  iconPaths: string[];
   name: WeaponNameByType[T];
+
+  private static BASE_ICON_URL = '../../../assets/weapons';
+  abstract ICON_URL_FOLDER: string;
 
   constructor(
     name: WeaponNameByType[T],
-    iconOrIcons: string | string[],
+    iconPathOrPaths: string | string[],
     buildsUpInto = new Set<Weapon<T>>(),
     defaultWeapon = false
   ) {
     this.buildsUpInto = buildsUpInto;
     this.defaultWeapon = defaultWeapon;
-    this.icons = typeof iconOrIcons === 'string' ? [iconOrIcons] : iconOrIcons;
+    this.iconPaths =
+      typeof iconPathOrPaths === 'string' ? [iconPathOrPaths] : iconPathOrPaths;
     this.name = name;
+  }
+
+  getCompleteIconPaths() {
+    return this.iconPaths.map(
+      (iconPath) =>
+        `${Weapon.BASE_ICON_URL}/${this.ICON_URL_FOLDER}/${iconPath}.webp`
+    );
   }
 }
