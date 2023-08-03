fb595c644e2d09449461893af20183552d4c68d3
Christopher <ChrFimbel@gmail.com>
2023-08-03 15:13:31 -0600

    Update WeaponAttributeName.ts

--- src/services/weapon/attributes/WeaponAttributeName.ts
+++ src/services/weapon/attributes/WeaponAttributeName.ts
@@ -1,3 +1,22 @@
-type WeaponAttributeName = 'attack' | 'endurance' | 'magicalPower' | 'speed';
+type WeaponAttributeName =
+  | 'antiBeast'
+  | 'antiDragon'
+  | 'antiMage'
+  | 'antiMarine'
+  | 'antiMetal'
+  | 'antiMimic'
+  | 'antiPlant'
+  | 'antiRock'
+  | 'antiSky'
+  | 'antiUndead'
+  | 'attack'
+  | 'endurance'
+  | 'fire'
+  | 'holy'
+  | 'ice'
+  | 'magicalPower'
+  | 'speed'
+  | 'thunder'
+  | 'wind';
 
 export default WeaponAttributeName;
