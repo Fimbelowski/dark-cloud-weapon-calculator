7eafa1a4bccb7c927e3a3de1080a16a21cdbd06c
Christopher <ChrFimbel@gmail.com>
2023-08-03 14:35:48 -0600

    WIP.

--- src/services/weapon/attributes/IWeaponAttributeCollection.ts
+++
@@ -1,9 +0,0 @@
-import type WeaponAttributeWithMin from './WeaponAttributeWithMin';
-import type WeaponAttributeWithMinAndMax from './WeaponAttributeWithMinAndMax';
-
-export default interface IWeaponAttributeCollection {
-  attack: WeaponAttributeWithMinAndMax;
-  endurance: WeaponAttributeWithMin;
-  magicalPower: WeaponAttributeWithMinAndMax;
-  speed: WeaponAttributeWithMin;
-}
