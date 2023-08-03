005e1ef72c4f87e310b03541276ae229206973e1
Christopher <ChrFimbel@gmail.com>
2023-08-03 15:20:35 -0600

    Update WeaponAttributeCollectionBuilder.ts

--- src/services/weapon/attributes/WeaponAttributeCollectionBuilder.ts
+++ src/services/weapon/attributes/WeaponAttributeCollectionBuilder.ts
@@ -7,17 +7,47 @@ import type WeaponAttributeName from './WeaponAttributeName';
 export default class WeaponAttributeCollectionBuilder
   implements IWeaponAttributeCollection
 {
+  antiBeast!: WeaponAttribute;
+  antiDragon!: WeaponAttribute;
+  antiMage!: WeaponAttribute;
+  antiMarine!: WeaponAttribute;
+  antiMetal!: WeaponAttribute;
+  antiMimic!: WeaponAttribute;
+  antiPlant!: WeaponAttribute;
+  antiRock!: WeaponAttribute;
+  antiSky!: WeaponAttribute;
+  antiUndead!: WeaponAttribute;
   attack!: WeaponAttribute;
   endurance!: WeaponAttribute;
+  fire!: WeaponAttribute;
+  holy!: WeaponAttribute;
+  ice!: WeaponAttribute;
   magicalPower!: WeaponAttribute;
   speed!: WeaponAttribute;
+  thunder!: WeaponAttribute;
+  wind!: WeaponAttribute;
 
   build() {
     return new WeaponAttributeCollection(
+      this.antiBeast,
+      this.antiDragon,
+      this.antiMage,
+      this.antiMarine,
+      this.antiMetal,
+      this.antiMimic,
+      this.antiPlant,
+      this.antiRock,
+      this.antiSky,
+      this.antiUndead,
       this.attack,
       this.endurance,
+      this.fire,
+      this.holy,
+      this.ice,
       this.magicalPower,
-      this.speed
+      this.speed,
+      this.thunder,
+      this.wind
     );
   }
 
