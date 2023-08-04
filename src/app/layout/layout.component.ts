afe9b98af7a9b62db6c05f1e5ae6d289d59c6647
Christopher <ChrFimbel@gmail.com>
2023-08-04 13:33:56 -0600

    Add state to layout component.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
 import swordGraph from 'src/services/weapon/sword/graph';
+import type Weapon from 'src/services/weapon/Weapon';
+import type WeaponType from 'src/services/weapon/WeaponType';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
+export class LayoutComponent<T extends WeaponType> {
+  // Ideally the layout component should be stateless, but in order to avoid
+  // polluting the codebase with a state-management library for the sake of two
+  // variables I'm adding that state here.
+  destinationWeapon?: Weapon<T>;
+  sourceWeapon?: Weapon<T>;
   weaponGraph = swordGraph;
 }
