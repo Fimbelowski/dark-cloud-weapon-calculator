b32ee10ad997ce820d5e0672c88f424ebd1da692
Christopher <ChrFimbel@gmail.com>
2023-08-04 12:31:11 -0600

    Add weapon graph to layout component.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import swordGraph from 'src/services/weapon/sword/graph';
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
-
+  weaponGraph = swordGraph;
 }
