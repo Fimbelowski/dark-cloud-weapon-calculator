3d5c75e00dab2089d263683d5a58490fa6e02a2a
Christopher <ChrFimbel@gmail.com>
2023-08-04 12:49:18 -0600

    Create termini-options component.

---
+++ src/app/termini-options/termini-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termini-options',
+  templateUrl: './termini-options.component.html',
+  styleUrls: ['./termini-options.component.scss']
+})
+export class TerminiOptionsComponent {
+
+}
