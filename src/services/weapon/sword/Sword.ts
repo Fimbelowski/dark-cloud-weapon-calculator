334bf8c169a632402442532f56e9cfad06b9f23c
Christopher <ChrFimbel@gmail.com>
2023-08-07 11:00:44 -0600

    Update Sword.ts

--- src/services/weapon/sword/Sword.ts
+++ src/services/weapon/sword/Sword.ts
@@ -1,9 +1,17 @@
+import type Cloneable from '../Cloneable';
 import Weapon from '../Weapon';
 
-export default class Sword extends Weapon<'sword'> {
+export default class Sword extends Weapon<'sword'> implements Cloneable<Sword> {
   ICON_URL_FOLDER = 'swords';
 
   constructor(...args: ConstructorParameters<typeof Weapon<'sword'>>) {
     super(...args);
+  }
+
+  clone() {
+    return new Sword(this.name, this.attributes.clone(), this.icons, {
+      buildsUpInto: this.buildsUpInto,
+      defaultWeapon: this.defaultWeapon,
+    });
   }
 }
