8a863d35a5fe543abaf331a5cfecc893852e4f87
Christopher <ChrFimbel@gmail.com>
2023-08-14 12:43:19 -0600

    Make Weapon class abstract.

--- src/services/weapons/Weapon.ts
+++ src/services/weapons/Weapon.ts
@@ -1,6 +1,6 @@
 import type WeaponNameByType from './WeaponNameByType';
 import type WeaponType from './WeaponType';
 
-export default class Weapon<T extends WeaponType> {
+export default abstract class Weapon<T extends WeaponType> {
   constructor(public readonly name: WeaponNameByType[T]) {}
 }
