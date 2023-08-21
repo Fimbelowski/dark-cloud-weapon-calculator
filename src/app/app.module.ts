4f3a892a62c9d67781e966101474e669042aaecb
Christopher <ChrFimbel@gmail.com>
2023-08-21 10:53:11 -0600

    Generate layout component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { WeaponGraphComponent } from './weapon-graph/weapon-graph.component';
 import { WeaponComponent } from './weapon/weapon.component';
 import { EdgeComponent } from './edge/edge.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent, EdgeComponent],
+  declarations: [AppComponent, WeaponGraphComponent, WeaponComponent, EdgeComponent, LayoutComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
