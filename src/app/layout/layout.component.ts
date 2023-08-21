4e07f25a1676e71cb4a6542e2e66f054a75794f5
Christopher <ChrFimbel@gmail.com>
2023-08-21 11:13:38 -0600

    Add content/styling to layout.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import graph from 'src/services/weapons/swords/graph';
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
-
+  weaponGraph = graph;
 }
