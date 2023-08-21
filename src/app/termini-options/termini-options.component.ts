6cda0029b7c28562db312e122513da2889507b4e
Christopher <ChrFimbel@gmail.com>
2023-08-21 11:20:01 -0600

    Stub out termini-options.

--- src/app/termini-options/termini-options.component.ts
+++ src/app/termini-options/termini-options.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import type Weapon from 'src/services/weapons/Weapon';
+import type WeaponType from 'src/services/weapons/WeaponType';
 
 @Component({
   selector: 'app-termini-options',
   templateUrl: './termini-options.component.html',
-  styleUrls: ['./termini-options.component.scss']
+  styleUrls: ['./termini-options.component.scss'],
 })
-export class TerminiOptionsComponent {
-
+export class TerminiOptionsComponent<T extends WeaponType> {
+  @Input() sourceWeapon?: Weapon<T>;
+  @Input() destinationWeapon?: Weapon<T>;
 }
