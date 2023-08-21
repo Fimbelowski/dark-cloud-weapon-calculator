d9ada87b7e48b7a8bc90262c7c2d3749063ad730
Christopher <ChrFimbel@gmail.com>
2023-08-21 11:13:12 -0600

    Don't encapsulate app root styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-
-import graph from 'src/services/weapons/swords/graph';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', '../sass/main.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  graph = graph;
   title = 'dark-cloud-weapon-calculator';
 }
