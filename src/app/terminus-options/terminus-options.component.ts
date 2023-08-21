d341b70a6be6921830587186eb59d12f594b79ae
Christopher <ChrFimbel@gmail.com>
2023-08-21 11:25:59 -0600

    Stub out temini-options.

--- src/app/terminus-options/terminus-options.component.ts
+++ src/app/terminus-options/terminus-options.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import type Weapon from 'src/services/weapons/Weapon';
+import type WeaponType from 'src/services/weapons/WeaponType';
 
 @Component({
   selector: 'app-terminus-options',
   templateUrl: './terminus-options.component.html',
-  styleUrls: ['./terminus-options.component.scss']
+  styleUrls: ['./terminus-options.component.scss'],
 })
-export class TerminusOptionsComponent {
-
+export class TerminusOptionsComponent<T extends WeaponType> {
+  @Input() weapon?: Weapon<T>;
 }
