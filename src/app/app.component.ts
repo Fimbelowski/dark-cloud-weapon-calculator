b8ec369c61a76aa5f2cd3f59e5b61ca5182c2982
Christopher <ChrFimbel@gmail.com>
2023-08-21 10:58:43 -0600

    Add SASS files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import graph from 'src/services/weapons/swords/graph';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss', '../sass/main.scss'],
 })
 export class AppComponent {
   graph = graph;
