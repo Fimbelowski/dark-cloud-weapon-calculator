0bd82b87365184b4f1eced1351dda72c04da5030
Christopher <ChrFimbel@gmail.com>
2023-08-23 08:58:08 -0600

    Finish up forms.

--- src/app/number-input/number-input.component.ts
+++ src/app/number-input/number-input.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-number-input',
   templateUrl: './number-input.component.html',
-  styleUrls: ['./number-input.component.scss']
+  styleUrls: ['./number-input.component.scss'],
 })
 export class NumberInputComponent {
-
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) label!: string;
+  @Input() max?: number;
+  @Input() min?: number;
+  @Input({ required: true }) value!: number;
 }
